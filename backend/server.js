fa250bedefe16ea31d95d9d3f5ecd2aa5f44b1cf
saikishore <makam.kishore.991.sandhya@gmail.com>
2025-07-04 19:52:58 +0530

    commit again

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ import cookieParser from "cookie-parser"
 
 const app = express();
 app.use(cors({
-    origin: ["http://localhost:5173", "https://help-desk-frontend-ashy.vercel.app"
+    origin: ["http://localhost:5173", "https://help-desk-frontend-bay.vercel.app"
     ],
     credentials:true
 }))
